eb4a8b1c7f0dc6689d9256e8fefa5059e0f58ffe
Young Jeong <youngjeong46@gmail.com>
2020-11-27 21:22:57 -0800

    minor lint edits

--- mail.py
+++ mail.py
@@ -1,43 +1,46 @@
 import config
-import smtplib, ssl
+import smtplib
+import ssl
 import getpass
 from email.mime.text import MIMEText
 
+
 def mail(sender, receiver, email, wishlist):
-  print(sender+" will send the gift to "+receiver+". Here is what they want: "+ wishlist)
-  host_email = config.host_email+"@"+config.email_domain
+    print(sender + " will send the gift to "+receiver+". Here is what they want: " + wishlist)
+    host_email = config.host_email + "@" + config.email_domain
   
-  msg = ('From: %s\r\nTo: %s\r\n' % (host_email, email))
-  msg = msg + '''Subject: [TEST] You have drawn a name!
+    msg = ('From: %s\r\nTo: %s\r\n' % (host_email, email))
+    msg = msg + '''Subject: [TEST] You have drawn a name!
 
-  Hi {name}, 
+    Hi {name}, 
+    
+    Thank you for participating in the season of gift giving! You are.... {receiver}'s Secret Unicorn! 
+    
+    Please look at their wish list here: {wishlist}.
+    
+    Thanks and let's have fun with this!!
+    
+    Sincerely Yours This Holiday,
+    
+    Secret Unicorn Host'''
   
-  Thank you for participating in the season of gift giving! You are.... {receiver}'s Secret Unicorn! 
-  
-  Please look at their wish list here: {wishlist}.
-  
-  Thanks and let's have fun with this!!
-  
-  Sincerely Yours This Holiday,
-  
-  Secret Unicorn Host'''
-  
-  port = 587
-  context = ssl.create_default_context()
+    port = 587
+    context = ssl.create_default_context()
 
-  user = smtplib.SMTP("smtp.gmail.com", port)
-  user.starttls(context=context)
-  while True:
-    password = getpass.getpass("Type your password and press enter: ")
-    try: 
-      user.login(host_email,password)
-      user.sendmail(host_email,email,msg.format(name=sender,receiver=receiver,wishlist=wishlist))
-      user.quit()
-    except smtplib.SMTPAuthenticationError:
-      print('Login incorrect: Please reenter the password')
-      continue
-    break
+    user = smtplib.SMTP("smtp.gmail.com", port)
+    user.starttls(context=context)
+    while True:
+        password = getpass.getpass("Type your password and press enter: ")
+        try:
+            user.login(host_email, password)
+            user.sendmail(host_email, email, msg.format(name=sender, receiver=receiver, wishlist=wishlist))
+            user.quit()
+        except smtplib.SMTPAuthenticationError:
+            print('Login incorrect: Please reenter the password')
+            continue
+        break
 
-if __name__=="__main__":
-  mail("Andrew","Young","test@mail.com","Unicorn Doll, Unicorn Ice Cream, Unicorn Rydes")
-  # mail_test()
+
+if __name__ == "__main__":
+    mail("Andrew", "Young", "test@mail.com", "Unicorn Doll, Unicorn Ice Cream, Unicorn Rydes")
+    # mail_test()
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import mail
-import csv, random, copy, sys
+import csv
+import random
+import copy
+import sys
+
 
 class Person:
 	def __init__(self, name, email, wishlist):
@@ -19,48 +23,54 @@ class Person:
 	def wishlist(self):
 		return self.__wishlist
 
-def enter_infos(csvList,domain):
-	infos={}
-	names=[]
-	f = open(csvList, 'r')
+
+def enter_info(csv_list, domain):
+	info = {}
+	names = []
+	f = open(csv_list, 'r')
 	reader = csv.reader(f)
-	headers = next(reader,None)
+	next(reader, None)
 
 	for row in reader:
 		names.append(row[0])
-		infos[row[0]]= Person(row[0],convert_emails(row[1],domain),row[2])
+		info[row[0]] = Person(row[0], convert_emails(row[1], domain), row[2])
 
-	return infos, names
+	return info, names
 
-def convert_emails(alias,domain):
+
+def convert_emails(alias, domain):
 	return alias+"@"+domain
+
 
 def draw_names(names):
 	my_list = names
 	choose = copy.copy(my_list)
 	selections = []
 	for i in my_list:
-			names = copy.copy(my_list)
-			names.pop(names.index(i))
-			chosen = random.choice(list(set(choose)&set(names)))
-			selections.append((i,chosen))
-			choose.pop(choose.index(chosen))
+		names = copy.copy(my_list)
+		names.pop(names.index(i))
+		chosen = random.choice(list(set(choose) & set(names)))
+		selections.append((i, chosen))
+		choose.pop(choose.index(chosen))
 	return selections
 
-def send_mails(infos,selections):
+
+def send_mails(info, selections):
 	for pair in selections:
 		sender = pair[0]
 		receiver = pair[1]
-		email = infos[sender].email()
-		wishlist = infos[receiver].wishlist()
-		mail.mail(sender,receiver,email,wishlist)
+		email = info[sender].email()
+		wishlist = info[receiver].wishlist()
+		mail.mail(sender, receiver, email, wishlist)
+
 
 def main(argv):
 	if not len(argv) == 2:
 		raise TypeError('This takes 2 arguments ({} given)'.format(len(argv)))
-	infos, names = enter_infos(argv[0], argv[1])
+	info, names = enter_info(argv[0], argv[1])
 	selections = draw_names(names)
-	send_mails(infos, selections)
+	send_mails(info, selections)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
 	main(sys.argv[1:])
